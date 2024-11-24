908c9bcfcfeff0f3af0a9b8b00cc50d652c3f7de
Yiwei Wang <2087842800@qq.com>
2024-11-25 01:47:20 +0800

    Add files via upload

---
+++ DeepLearning/MNIST/MNIST-basic/mnist-basic.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import matplotlib.pyplot as plt
+
+# 加载MNIST数据集
+mnist = tf.keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+# 数据预处理
+train_images = train_images.reshape((60000, 28, 28, 1)).astype('float32') / 255
+test_images = test_images.reshape((10000, 28, 28, 1)).astype('float32') / 255
+
+# 将标签转换为one-hot编码
+train_labels = tf.keras.utils.to_categorical(train_labels)
+test_labels = tf.keras.utils.to_categorical(test_labels)
+
+# 构建卷积神经网络模型
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+# 编译模型
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# 训练模型
+model.fit(train_images, train_labels, epochs=5, batch_size=64, validation_split=0.1)
+
+# 评估模型
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(f'Test accuracy: {test_acc:.4f}')
+
+# 预测9个检测样本
+predictions = model.predict(test_images[:9])
+
+# 展示9个检测样本及其预测结果
+plt.figure(figsize=(10, 10))
+for i in range(9):
+    plt.subplot(3, 3, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(test_images[i].reshape(28, 28), cmap=plt.cm.binary)
+    predicted_label = tf.argmax(predictions[i]).numpy()
+    true_label = tf.argmax(test_labels[i]).numpy()
+    plt.xlabel(f"Pred: {predicted_label} (True: {true_label})")
+plt.show()
